ece1ed898718cbf2b691cb7660cf09bdc7e04322
engsaramo <446000253@pnu.edu.sa>
2025-10-20 14:39:47 +0300

    Initial Commit

---
+++ Second app/Second_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_appApp.swift
+//  Second app
+//
+//  Created by Sarah Aleissa on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Second_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
